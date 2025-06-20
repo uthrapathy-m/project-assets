32639a945f057089375efe43ec490518ac000196
Uthra <mail.uthrapathy@gmail.com>
2025-06-20 11:00:27 +0530

    Update EKS Infra Variables

--- EKS - INFRASTRUCTURE - TERRAFORM/output.tf
+++ EKS - INFRASTRUCTURE - TERRAFORM/output.tf
@@ -1,16 +1,16 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.cicd-project.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.cicd-project.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.cicd-project_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.cicd-project_subnet[*].id
 }
 
